31c4eda9f976b318219bec6999d90e2c326cc501
André <luizandre23012007@gmail.com>
2025-08-27 11:04:51 -0300

    Add files via upload

---
+++ Light and dark theme/tema.js
@@ -0,0 +1,17 @@
+
+let switcher  = document.querySelector('.btn')
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+let className = document.body.className
+if (className == 'dark-theme') {
+    this.textContent = "Dark"
+} 
+else { 
+    this.textContent = "Light"
+    console.log('current class name: ' + className)
+}
+})
+
+
